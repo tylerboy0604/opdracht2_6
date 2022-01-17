8d90e73b22c142b0da6a2b7d6a021159dfc6f0d4
tylerman123 <77289575+tylerman123@users.noreply.github.com>
2022-01-17 09:54:21 +0100

    slide show wisselen

--- index.js
+++ index.js
@@ -10,4 +10,6 @@ window.addEventListener('load', slideshowSetUp);
 
 function slideshowNext() { 
     let slides = document.getElementsByClassName('slide');
+    slides[0].style.opacity = 0;
+    slides[1].style.opacity = 1;
 }
