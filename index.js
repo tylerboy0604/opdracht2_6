92ef9e353e86f16a0fceffe6d6c3dcff8fec42ba
tylerman123 <77289575+tylerman123@users.noreply.github.com>
2022-01-17 10:02:22 +0100

    classlists

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ window.addEventListener('load', slideshowSetUp);
 
 function slideshowNext() { 
     let slides = document.getElementsByClassName('slide');
-    slides[0].style.opacity = 0;
-    slides[1].style.opacity = 1;
+
+
+
+    slides[0].classList.remove('visible');
+    slides[1].classList.add('visible');
 }
